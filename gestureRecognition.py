50bf8982f562a55483bf08f1f51900a8848bf2c3
Susan Fox <fox@macalester.edu>
2018-04-06 18:15:25 -0500

    Modified to work with lab machine

--- gestureRecognition.py
+++ gestureRecognition.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-
+print(cv2.__version__)
 vidCap = cv2.VideoCapture(0)
 
 top_left = (50, 100)
@@ -63,7 +63,7 @@ while True:
 
 
     #Finding the biggest contour
-    _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
     max_area = 0
     for contour in contours:

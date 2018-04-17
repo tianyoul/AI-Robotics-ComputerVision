d17d0afeaa5b263677eb522f2c7e10ecf476612b
tianyoul <youzi323@live.cn>
2018-04-17 16:14:45 -0500

    Try on Lab computer

--- gather_data.py
+++ gather_data.py
@@ -8,6 +8,10 @@ vidCap = cv2.VideoCapture(0)
 
 # variables
 
+lower_threshold = np.array([0, 35, 35], dtype="uint8")
+upper_threshold = np.array([22, 255, 255], dtype="uint8")
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6, 6))
+
 # The square area (500 * 500)
 top_left = (50, 100)
 bottom_right = (550, 600)  # openCV order
@@ -21,14 +25,15 @@ dir_prefix = "img/"
 label_folder = ['fist', 'hand', 'peace', 'none']
 
 
-data_to_load = ['test'] #choose which data to load
+
+data_to_load = ['left'] #choose which data to load
 
 i = 0
 while i < len(data_to_load):
     x = cv2.waitKey(10)
     ret, vd_img = vidCap.read()
     cv2.rectangle(vd_img, top_left, bottom_right, green)
-    cv2.putText(vd_img, "Press C to capture " + label_folder[i] + " data", (5, 30), font, 1, (255, 255, 255), 3)
+    cv2.putText(vd_img, "Press C to capture " + data_to_load[i] + " data", (5, 30), font, 1, (255, 255, 255), 3)
     cv2.imshow("background", vd_img)
 
     char = chr(x & 0xFF)
@@ -45,7 +50,14 @@ while i < len(data_to_load):
             #Currently not displaying progress
             cv2.imshow("background", vd_img)
             area_img = vd_img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-            cv2.imwrite(folder_path + "/" + data_to_load[i] + str(j) + ".jpg", area_img)
+
+            skin_mask = cv2.inRange(area_img, lower_threshold, upper_threshold)
+
+            # apply a series of erosion and dilation
+            skin_mask = cv2.dilate(skin_mask, kernel, iterations=4)
+
+
+            cv2.imwrite(folder_path + "/" + data_to_load[i] + str(j) + ".jpg", skin_mask)
 
         i += 1 # load next gesture
 
---
+++ realtime_detect.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+classes = ['fist', 'hand', 'none', 'peace']
+
+def find_gesture(result):
+    result = result[0]
+    max_val = 0.0
+    max_ind = 0
+    for i in range(len(result)):
+        if result[i] > max_val:
+            max_val = result[i]
+            max_ind = i
+
+    return max_ind
+
+## Let us restore the saved model
+sess = tf.Session()
+# Step-1: Recreate the network graph. At this step only graph is created.
+saver = tf.train.import_meta_graph('model.meta')
+# Step-2: Now let's load the weights saved using the restore method.
+saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+# Accessing the default graph which we have restored
+graph = tf.get_default_graph()
+
+# Now, let's get hold of the op that we can be processed to get the output.
+# In the original network y_pred is the tensor that is the prediction of the network
+y_pred = graph.get_tensor_by_name("y_pred:0")
+
+
+
+
+vidCap = cv2.VideoCapture(0)
+
+top_left = (50, 100)
+bottom_right = (500, 600)  # In opencv, need to be reversed in np
+green = (100, 180, 50)
+num_channels=3
+image_size = 500
+
+while True:
+    x = cv2.waitKey(10)
+    char = chr(x & 0xFF)
+    if char == 'q':
+        break
+    ret, img = vidCap.read()
+
+    cv2.rectangle(img, top_left, bottom_right, green)
+    gesture = img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+    images = []
+    # Resizing the image to our desired size and preprocessing will be done exactly as done during training
+    image = cv2.resize(gesture, (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
+    images.append(image)
+    images = np.array(images, dtype=np.uint8)
+    images = images.astype('float32')
+    images = np.multiply(images, 1.0 / 255.0)
+    x_batch = images.reshape(1, image_size, image_size, num_channels)
+
+    ## Let's feed the images to the input placeholders
+    x = graph.get_tensor_by_name("x:0")
+    y_true = graph.get_tensor_by_name("y_true:0")
+    y_test_images = np.zeros((1, 4))
+
+    feed_dict_testing = {x: x_batch, y_true: y_test_images}
+    result = sess.run(y_pred, feed_dict=feed_dict_testing)
+    print(classes[find_gesture(result)])
+    cv2.imshow("Camera", img)
+
+cv2.destroyAllWindows()
+vidCap.release()
